7006402f6a8d8b86b37515eabf3f180938c900fc
Camden Bickel <cam@bickel.io>
2018-07-17 09:27:50 -0400

    added dummy info section

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,27 @@ import styled from 'react-emotion';
 import styles from 'config/styles';
 
 const Header = () => {
-  return <Name>Camden Bickel</Name>;
+  return (
+    <Wrapper>
+      <Name>Camden Bickel</Name>
+      <Info>info here</Info>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  margin-top: 0.5em;
+`;
 
 const Name = styled.h2`
   font-size: ${styles.fontSize.xLarge};
-  margin-top: 0.25em;
+  margin-top: 0;
+`;
+
+const Info = styled.div`
+  flex: 1;
+  text-align: right;
 `;
 
 export default Header;
