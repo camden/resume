0c41de6df794f87aaaaa678b77ad857c7ace58fe
Camden Bickel <cam@bickel.io>
2018-08-02 16:02:17 -0400

    added .env warning

--- src/utils/mergeEnvData.js
+++ src/utils/mergeEnvData.js
@@ -1,4 +1,20 @@
 const mergeEnvData = infoData => {
+  const missingLabels = [
+    'INFO_PHONE',
+    'INFO_EMAIL',
+    'INFO_LOCATION_CITY',
+    'INFO_LOCATION_REGION',
+    'INFO_LOCATION_COUNTRY',
+  ].filter(label => !process.env[label]);
+
+  if (missingLabels.length > 0) {
+    console.groupCollapsed(
+      "Looks like you're missing some info in your .env file!"
+    );
+    missingLabels.forEach(label => console.log(`Missing: ${label}`));
+    console.groupEnd();
+  }
+
   return infoData
     .set('phone', process.env.INFO_PHONE)
     .set('email', process.env.INFO_EMAIL)
