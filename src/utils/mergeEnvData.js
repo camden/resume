4c81fcf68b55ebbfec078c2579cab9e9364bfec8
Camden Bickel <cam@bickel.io>
2019-07-24 15:14:40 -0700

    Update mergeEnvData.js

--- src/utils/mergeEnvData.js
+++ src/utils/mergeEnvData.js
@@ -1,10 +1,10 @@
 const mergeEnvData = infoData => {
   const missingLabels = [
-    'INFO_PHONE',
-    'INFO_EMAIL',
-    'INFO_LOCATION_CITY',
-    'INFO_LOCATION_REGION',
-    'INFO_LOCATION_COUNTRY',
+    'GATSBY_INFO_PHONE',
+    'GATSBY_INFO_EMAIL',
+    'GATSBY_INFO_LOCATION_CITY',
+    'GATSBY_INFO_LOCATION_REGION',
+    'GATSBY_INFO_LOCATION_COUNTRY',
   ].filter(label => !process.env[label]);
 
   if (missingLabels.length > 0) {
@@ -14,15 +14,13 @@ const mergeEnvData = infoData => {
     missingLabels.forEach(label => console.log(`Missing: ${label}`));
     console.groupEnd();
   }
-
-  console.log(JSON.stringify(process.env), process.env)
   
   return infoData
-    .set('phone', process.env.INFO_PHONE)
-    .set('email', process.env.INFO_EMAIL)
-    .setIn(['location', 'city'], process.env.INFO_LOCATION_CITY)
-    .setIn(['location', 'region'], process.env.INFO_LOCATION_REGION)
-    .setIn(['location', 'countryCode'], process.env.INFO_LOCATION_COUNTRY);
+    .set('phone', process.env.GATSBY_INFO_PHONE)
+    .set('email', process.env.GATSBY_INFO_EMAIL)
+    .setIn(['location', 'city'], process.env.GATSBY_INFO_LOCATION_CITY)
+    .setIn(['location', 'region'], process.env.GATSBY_INFO_LOCATION_REGION)
+    .setIn(['location', 'countryCode'], process.env.GATSBY_INFO_LOCATION_COUNTRY);
 };
 
 export default mergeEnvData;
